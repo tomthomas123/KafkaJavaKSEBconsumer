c7c5b2a73d3e8b2738c0f2cc9a8cf610a01d9632
tomthomas123 <tomthomas1248@gmail.com>
2022-11-11 12:48:56 +0530

    development added

--- src/Consumer.java
+++ src/Consumer.java
@@ -1,3 +1,33 @@
-public class Consumer {
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 
+import java.util.Arrays;
+import java.util.Properties;
+
+public class Consumer {
+    public static void main(String[] args) {
+        KafkaConsumer consumer;
+        String topic = "kseb";
+        String broker = "localhost:9092";
+        Properties props = new Properties();
+        props.put("bootstrap.servers",broker);
+        props.put("group.id", "test-group");
+        props.put("key.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        consumer = new KafkaConsumer(props);
+        consumer.subscribe(Arrays.asList(topic));
+
+        while(true){
+            ConsumerRecords<String,String> records = consumer.poll(100);
+            for(ConsumerRecord<String,String> record : records){
+                System.out.println(record.value());
+
+            }
+
+        }
+
+
+
+    }
 }
