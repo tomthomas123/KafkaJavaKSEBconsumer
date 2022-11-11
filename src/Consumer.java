579321a24c6c9be28d4b29e2f0a1b7daa1035b0a
tomthomas123 <tomthomas1248@gmail.com>
2022-11-11 16:19:55 +0530

    jsondata added

--- src/Consumer.java
+++ src/Consumer.java
@@ -1,14 +1,15 @@
+import netscape.javascript.JSObject;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-
+import org.json.JSONObject;
+import java.sql.*;
 import java.util.Arrays;
 import java.util.Properties;
-
 public class Consumer {
     public static void main(String[] args) {
         KafkaConsumer consumer;
-        String topic = "kseb";
+        String topic = "ksebtopic";
         String broker = "localhost:9092";
         Properties props = new Properties();
         props.put("bootstrap.servers",broker);
@@ -17,11 +18,36 @@ public class Consumer {
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         consumer = new KafkaConsumer(props);
         consumer.subscribe(Arrays.asList(topic));
-
         while(true){
             ConsumerRecords<String,String> records = consumer.poll(100);
             for(ConsumerRecord<String,String> record : records){
                 System.out.println(record.value());
+                System.out.println(String.valueOf(record.value()));
+                JSONObject obj = new JSONObject(record.value());
+                String userId = String.valueOf(obj.getInt("userid"));
+                String unit = String.valueOf(obj.getInt("unit"));
+                System.out.println(userId);
+                System.out.println(unit);
+                //adding db
+                try{
+                    Class.forName("com.mysql.jdbc.Driver");
+                    Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ksebdb","root","");
+                    String sql = "INSERT INTO `usages`( `user_id`, `unit`, `date`) VALUES (?,?,now())";
+                    PreparedStatement stmt = con.prepareStatement(sql);
+                    stmt.setString(1,userId);
+                    stmt.setString(2,unit);
+                    stmt.executeUpdate();
+                    con.close();
+                }
+                catch (Exception e){
+                    System.out.println(e);
+                }
+
+
+
+
+
+
 
             }
 
